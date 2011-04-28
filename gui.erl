e6877427c5a7b901024be25e83bd352a45398d48
Olivier Girondel <olivier@biniou.info>
2011-04-28 21:33:28 +0200

    Move drawing code to screen

--- gui.erl
+++ gui.erl
@@ -15,10 +15,6 @@
 %% wx_object API
 -export([start_link/0]).
 
-%% module API
-%% -export([get_env/0, register/1, unregister/1]).
-%% -export([register_texture/2, unregister_texture/2, unregister_textures/1]).
-
 %% wx_object callbacks
 -export([init/1]).
 
@@ -26,8 +22,6 @@
 %%-export([system_code_change/4, system_continue/3]).
 
 -define(SERVER, ?MODULE).
-
--define(ZMAX, 5).
 
 -record(state,  {ifps, gl, size}).
 
@@ -39,35 +33,6 @@ start() ->
 
 start_link() ->
     proc_lib:start_link(?MODULE, init, [self()]).
-
-
-%% get_env() ->
-%%     Ref = make_ref(),
-%%     ?SERVER ! {self(), Ref, get_env},
-%%     receive
-%% 	{Ref, Env} ->
-%% 	    Env
-%%     end.
-
-
-%% register(Pid) ->
-%%     ?SERVER ! {register, Pid}.
-
-
-%% unregister(Pid) ->
-%%     ?SERVER ! {unregister, Pid}.
-
-
-%% register_texture(Pid, Tid) ->
-%%     ?SERVER ! {register_texture, Pid, Tid}.
-
-
-%% unregister_texture(Pid, Tid) ->
-%%     ?SERVER ! {unregister_texture, Pid, Tid}.
-
-
-%% unregister_textures(Pid) ->
-%%     ?SERVER ! {unregister_textures, Pid}.
 
 
 %%====================================================================
@@ -104,47 +69,10 @@ init(Parent) ->
 %% Internal functions
 %%====================================================================
 loop(Parent, Debug, #state{ifps=IFPS} = State) ->
-    %%?D_F("*** LOOP", []),
     receive
-	%% {Pid, Ref, get_env} ->
-	%%     Pid ! {Ref, wx:get_env()},
-	%%     loop(Parent, Debug, State);
-
-	%% {register, Pid} ->
-	%%     link(Pid),
-        %%     Plugins = [Pid|State#state3.plugins],
-	%%     loop(Parent, Debug, State#state3{plugins=Plugins});
-
-	%% {unregister, Pid} ->
-	%%     unlink(Pid),
-	%%     {ok, NewState} = unregister(Pid, State),
-	%%     loop(Parent, Debug, NewState);
-
-	%% {register_texture, Pid, Tid} ->
-	%%     ets:insert(Texs, {Pid, Tid}),
-	%%     loop(Parent, Debug, State);
-
-	%% {unregister_texture, Pid, Tid} ->
-	%%     ets:delete_object(Texs, {Pid, Tid}),
-	%%     wxGLCanvas:setCurrent(GL),
-	%%     gl:deleteTextures([Tid]),
-	%%     loop(Parent, Debug, State);
-
-	%% {unregister_textures, Pid} ->
-	%%     Records = ets:lookup(Texs, Pid),
-	%%     ets:delete(Texs, Pid),
-	%%     Tids = [Tid || {_Pid, Tid} <- Records],
-	%%     ?D_F("freeing textures owned by pid ~p: ~p", [Pid, Tids]),
-	%%     wxGLCanvas:setCurrent(GL),
-	%%     gl:deleteTextures(Tids),
-	%%     loop(Parent, Debug, State);
-
 	draw ->
-	    %%?D_F("+++ draw~n", []),
-	    draw(State),
-	    %%?D_F("+++ tick~n", []),
+	    draw(),
 	    tick(IFPS),
-	    %%?D_F("+++ loop~n", []),
 	    loop(Parent, Debug, State);
 
 	{'EXIT', Pid, Reason} ->
@@ -171,78 +99,9 @@ loop(Parent, Debug, #state{ifps=IFPS} = State) ->
     end.
 
 
-draw(#state{gl=GL, size=Size} = _State) ->
-    %%?D_F("*** draw(~p)~n", [_State]),
-    wxGLCanvas:setCurrent(GL),
-    set_view(Size),
-    wirecube:draw(),
-    %%NewState = draw2(State),
-    wxGLCanvas:swapBuffers(GL).
-    %%NewState.
-
-
-%% draw2(State) ->
-%%     draw2(State, State#state.plugins, []).
-%% draw2(State, [], Acc) ->
-%%     State#state{plugins=lists:reverse(Acc)};
-%% draw2(State, [Pid|Pids], Acc) ->
-%%     Ref = make_ref(),
-%%     Pid ! {self(), Ref, {draw, State#state.gl}},
-%%     receive
-%%         {Ref, _Result} ->
-%%             draw2(State, Pids, [Pid|Acc]);
-
-%%         {'EXIT', Pid, _Reason} ->
-%%             ?D_F("plugin ~p crashed, reason: ~p", [Pid, _Reason]),
-%%             {ok, NewState} = unregister(Pid, State),
-%%             draw2(NewState, Pids, Acc)
-%%     end.
-
-
-%% TODO: bouger dans le screen
-set_view({Width, Height}) ->
-    gl:shadeModel(?GL_SMOOTH),
-    gl:depthFunc(?GL_LEQUAL),
-    gl:enable(?GL_DEPTH_TEST),
-    gl:enable(?GL_BLEND),
-    gl:clearColor(0.0, 0.0, 0.0, 1.0),
-    gl:clearDepth(?ZMAX),
-
-    gl:matrixMode(?GL_PROJECTION),
-    gl:loadIdentity(),
-
-    Ratio = Width / Height,
-
-    %% FIXME TODO: dans la conf du screen
-    glu:perspective(?DEFAULT_FOV, Ratio, 0.1, ?ZMAX),
-    glu:lookAt(0, 0, 3.14,
-	       0, 0, -3.14,
-	       0, 1, 0),
-
-    %% FIXME
-    {RotX, RotY, RotZ} = ?DEFAULT_ROT, %%ec_cf:rot(),
-    gl:rotatef(RotX, 1.0, 0.0, 0.0),
-    gl:rotatef(RotY, 0.0, 1.0, 0.0),
-    gl:rotatef(RotZ, 0.0, 0.0, 1.0),
-
-    gl:clear(?GL_COLOR_BUFFER_BIT bor ?GL_DEPTH_BUFFER_BIT).
+draw() ->
+    screen:draw().
 
 
 tick(T) ->
     erlang:send_after(T, ?SERVER, draw).
-
-
-%% unregister(Pid, #state{plugins=Plugins} = State) ->
-%%     case lists:member(Pid, Plugins) of
-%%         true ->
-%%             NPlugins = Plugins -- [Pid],
-%%             unregister_textures(Pid),
-%% 	    {ok, State#state3{plugins=NPlugins}};
-
-%%         false ->
-%% 	    {false, State}
-%%     end.
-
-
-%% ets() ->
-%%     ets:new(?SERVER, [duplicate_bag, private]).
--- screen.erl
+++ screen.erl
@@ -11,17 +11,37 @@
 
 -behaviour(wx_object).
 
+%% API
+-export([draw/0]).
+
 %% wx_object API
 -export([new/2]).
 
 %% wx_object callbacks
--export([init/1, handle_event/2, terminate/2]).
+-export([init/1, handle_call/3, handle_event/2, terminate/2]).
 
 -define(SERVER, ?MODULE).
 
 %% GL widget state
--record(state, {frame, gl, mouse}).
+-record(state, {size, frame, gl, mouse}).
 
+-define(ZMAX, 5).
+
+%%   handle_call(Msg, {From, Tag}, State) should return <br/>
+%%    {reply, Reply, State} | {reply, Reply, State, Timeout} |
+%%        {noreply, State} | {noreply, State, Timeout} |
+%%        {stop, Reason, Reply, State}  
+
+draw() ->
+    wx_object:call(?SERVER, draw).
+
+handle_call(draw, _From, #state{size=Size, gl=GL} = State) ->
+    %% ?D_F("======= screen / call~n", []),
+    wxGLCanvas:setCurrent(GL),
+    set_view(Size),
+    wirecube:draw(),
+    wxGLCanvas:swapBuffers(GL),
+    {reply, ok, State}.
 
 new(Frame, Size) ->
     wx_object:start_link(?MODULE, [Frame, Size], []).
@@ -43,7 +63,7 @@ init([Frame, Size]) ->
 
     ?D_REGISTER(?SERVER, self()), %% not needed
 
-    {GL, #state{frame=Frame, gl=GL}}.
+    {GL, #state{size=Size, frame=Frame, gl=GL}}.
 
 
 handle_event(#wx{event=#wxMouse{type=left_down, x=X, y=Y}}, State) ->
@@ -121,3 +141,31 @@ handle_event(#wx{event=#wxKey{}}, State) ->
 
 terminate(_Reason, _State) ->
     ?D_TERMINATE(_Reason).
+
+
+set_view({Width, Height}) ->
+    gl:shadeModel(?GL_SMOOTH),
+    gl:depthFunc(?GL_LEQUAL),
+    gl:enable(?GL_DEPTH_TEST),
+    gl:enable(?GL_BLEND),
+    gl:clearColor(0.0, 0.0, 0.0, 1.0),
+    gl:clearDepth(?ZMAX),
+
+    gl:matrixMode(?GL_PROJECTION),
+    gl:loadIdentity(),
+
+    Ratio = Width / Height,
+
+    %% FIXME TODO: dans la conf du screen
+    glu:perspective(?DEFAULT_FOV, Ratio, 0.1, ?ZMAX),
+    glu:lookAt(0, 0, 3.14,
+	       0, 0, -3.14,
+	       0, 1, 0),
+
+    %% FIXME
+    {RotX, RotY, RotZ} = ?DEFAULT_ROT, %%ec_cf:rot(),
+    gl:rotatef(RotX, 1.0, 0.0, 0.0),
+    gl:rotatef(RotY, 0.0, 1.0, 0.0),
+    gl:rotatef(RotZ, 0.0, 0.0, 1.0),
+
+    gl:clear(?GL_COLOR_BUFFER_BIT bor ?GL_DEPTH_BUFFER_BIT).
