6bc46cf92c75821364a71f732971f6022c000582
Olivier Girondel <olivier@biniou.info>
2014-05-31 17:50:54 +0200

    Remove unused function msplit/2.

--- takens.erl
+++ takens.erl
@@ -2,7 +2,6 @@
 -author('olivier@biniou.info').
 
 -export([embed3/1]).
--export([msplit/2]). %% TODO: msplit/3 avec une taille minimum ?
 
 
 %% Takens embedding (dim=3, delay=1)
@@ -14,16 +13,3 @@ embed3([X,Y,Z|Tail], Acc) ->
     embed3([Y,Z|Tail], [Point|Acc]);
 embed3(_Rest, Acc) ->
     Acc.
-
-
-%% multi-split
-msplit(Size, List) ->
-    msplit(Size, List, []).
-
-msplit(Size, List, Acc) when length(List) >= Size ->
-    {Chunk, Rest} = lists:split(Size, List),
-    msplit(Size, Rest, [Chunk|Acc]);
-msplit(_Size, [], Acc) ->
-    lists:reverse(Acc);
-msplit(_Size, Rest, Acc) ->
-    lists:reverse([Rest|Acc]).
