8f93e537b4a3bfca4a3cfa463f4655ffc2a07121
Olivier Girondel <olivier@biniou.info>
2014-06-15 23:59:09 +0200

    First working sketch.

--- particle_system.erl
+++ particle_system.erl
@@ -11,14 +11,21 @@
 
 -export([draw/0]).
 -export([create/1]).
+-export([remove/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
+-export([cloud/1]).
 
 -define(SERVER, ?MODULE).
+-define(TABLE, clouds).
 
--record(state, {}).
+-record(state, {pids = []}).
+
+-define(TTL, 2000). %% Time To Leave, 3secs.
+-define(STEP, 200).
+-record(cloud, {age = 0, particles}).
 
 %%%===================================================================
 %%% API
@@ -28,6 +35,9 @@ draw() ->
 
 create(Particles) ->
     gen_server:cast(?SERVER, {create, Particles}).
+
+remove(Pid) ->
+    gen_server:cast(?SERVER, {remove, Pid}).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -59,7 +69,8 @@ stop() ->
 %% @end
 %%--------------------------------------------------------------------
 init([Env, GL]) ->
-    io:format("Starting particle system, Env= ~p~n", [Env]),
+    Tid = ets:new(?TABLE, [named_table, public]),
+    io:format("Starting particle system, Env= ~p Tid= ~p~n", [Env, Tid]),
     wx:set_env(Env),
     wxGLCanvas:setCurrent(GL),
     {ok, #state{}}.
@@ -78,9 +89,12 @@ init([Env, GL]) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_call(draw, _From, State) ->
-    Reply = draw_cb(),
-    {reply, Reply, State};
+handle_call(draw, _From, #state{pids = Pids} = State) ->
+    gl:pointSize(1.0),
+    gl:'begin'(?GL_POINTS),
+    [draw_cb(Pid) || Pid <- Pids],
+    gl:'end'(),
+    {reply, ok, State};
 
 handle_call(stop, _From, State) ->
     io:format("Stopping particle system~n"),
@@ -101,8 +115,22 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast({create, Particles0}, #state{pids = Pids} = State) ->
+    Particles = make_particles(Particles0),
+    Pid = spawn(?MODULE, cloud, [Particles]),
+    ets:insert(?TABLE, {Pid, Particles}),
+    NewPids = [Pid | Pids],
+    %% io:format("New cloud, pids= ~p~n", [length(NewPids)]),
+    {noreply, State#state{pids = NewPids}};
+
+handle_cast({remove, Pid}, #state{pids = Pids} = State) ->
+    NewPids = Pids -- [Pid],
+    ets:delete(?TABLE, Pid),
+    %% io:format("Done cloud, pids= ~p~n", [length(NewPids)]),
+    {noreply, State#state{pids = NewPids}};
+
 handle_cast(_Msg, State) ->
-    %% io:format("unknown cast~n"),
+    io:format("unknown cast ~w~n", [_Msg]),
     {noreply, State}.
 
 %%--------------------------------------------------------------------
@@ -147,9 +175,68 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-draw_cb() ->
-    gl:pushMatrix(),
-    R = random:uniform() * 0.1 + 1,
-    gl:scalef(R, R, R),
-    wirecube:draw({0.0, 0.0, 1.0}),
-    gl:popMatrix().
+%% draw_cb_old() ->
+%%     gl:pushMatrix(),
+%%     R = random:uniform() * 0.1 + 1,
+%%     gl:scalef(R, R, R),
+%%     wirecube:draw({0.0, 0.0, 1.0}),
+%%     gl:popMatrix().
+
+
+draw_cb(Pid) ->
+    %% draw_cb_old(),
+    case ets:lookup(?TABLE, Pid) of
+	[{Pid, Particles}] ->
+	    %% io:format("found ~p particles for pid ~p~n", [length(Particles), Pid]),
+	    [draw_particle(P) || P <- Particles];
+	Other ->
+	    io:format("Other in draw_cb: ~p~n", [Other]),
+	    halt(1)
+    end.
+
+
+draw_particle(#{pos := Pos, col := Col}) ->
+    gl:color3fv(Col),
+    gl:vertex3fv(Pos).
+
+
+make_particles(P) ->
+    make_particles(P, []).
+make_particles([], Acc) ->
+    Acc;
+make_particles([{Pos, Col} | Ps], Acc) ->
+    P = #{pos => Pos, vel => vel(Pos), col => Col},
+    make_particles(Ps, [P | Acc]).
+
+
+-define(FACT, 0.1).
+
+vel({X, Y, Z}) ->
+    {X * ?FACT, Y * ?FACT, Z * ?FACT}.
+
+
+%% TODO: virer le particles du state,
+%% Le process recupere la liste de particles avec un ets:lookup
+cloud(Particles) when is_list(Particles) ->
+    cloud(#cloud{particles = Particles});
+cloud(#cloud{age = Age}) when Age > ?TTL ->
+    Me = self(),
+    %% io:format("--- Cloud ~p exiting~n", [Me]),
+    remove(Me),
+    ok;
+cloud(#cloud{age = Age, particles = Particles}) ->
+    %% io:format("new cloud ~p~n", [Particles]),
+    timer:sleep(?STEP),
+    NewAge = Age + ?STEP,
+    %% io:format("*** Cloud ~p age ~p~n", [self(), NewAge]),
+    NewParticles = [move(P) || P <- Particles],
+    ets:insert(?TABLE, {self(), NewParticles}),
+    cloud(#cloud{age = NewAge, particles = NewParticles}).
+
+
+move(#{pos := Pos, vel := Vel} = P) ->
+    P#{pos => add(Pos, Vel)}.
+
+
+add({A, B, C}, {D, E, F}) ->
+    {A+D, B+E, C+F}.
